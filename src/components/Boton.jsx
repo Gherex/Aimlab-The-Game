180ba357551b36c659a502e6583df5689148b087
Gherex <germilagger@gmail.com>
2024-10-06 04:39:29 -0300

    Cambios en componente Boton

--- src/components/Boton.jsx
+++ src/components/Boton.jsx
@@ -1,10 +1,9 @@
-
 function Boton({ children }) {
   return (
     <button className="bg-cyan-950 w-40 h-20 rounded-2xl border-2 font-bold text-2xl hover:bg-cyan-600 select-none duration-300">
       {children}
     </button>
-  )
+  );
 }
 
-export default Boton
+export default Boton;
